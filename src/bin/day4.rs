5f0e7d66d793f2987553b7eac21af444653f24ae
Aidan Goldthorpe <agoldthorpe@atlassian.com>
2021-12-06 18:45:04 +1100

    Day 4 part 1

---
+++ src/bin/day4.rs
@@ -0,0 +1,90 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use itertools::FoldWhile::Continue;
+use itertools::FoldWhile::Done;
+
+use itertools::Itertools;
+
+fn main() {
+    let f = File::open("input/input4.txt").unwrap();
+    let reader = BufReader::new(f);
+    let mut lines = reader.lines();
+    let moves = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split(",")
+        .map(|val| val.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+    let boards = lines.chunks(6).into_iter().map(|mut chunk| {
+      // skip a blank line
+      chunk.next();
+      chunk.map(|line| {
+        line.unwrap().split_whitespace().map(|x| x.parse::<u32>().unwrap()).collect::<Vec<_>>()
+      }).flatten().collect::<Vec<_>>()
+    }).collect::<Vec<_>>();
+
+    let mut winning_board = 0;
+    let mut last_number = 0;
+    let played_numbers = moves.into_iter().fold_while(HashSet::new(), |mut played_numbers, x| {
+      played_numbers.insert(x);
+      let maybe_winning_board = boards.clone()
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, b)| {
+          if is_board_complete(&b, &played_numbers) {
+            return Some(i)
+          }
+          None
+        })
+        .next();
+      if maybe_winning_board.is_some() {
+        winning_board = maybe_winning_board.unwrap();
+        last_number = x;
+        return Done(played_numbers);
+      }
+      Continue(played_numbers)
+    }).into_inner();
+
+    println!("{:?} won, played numbers were {:?}", winning_board, played_numbers);
+
+    println!("Score is {}", calculate_score(&boards[winning_board], &played_numbers, last_number));
+
+}
+
+fn is_board_complete(board: &Vec<u32>, numbers: &HashSet<u32>) -> bool {
+  // check rows
+  for row in 0..5 {
+    let mut complete = true;
+    for col in 0..5 {
+      if !(numbers.contains(&board[row * 5 + col])) {
+        complete = false;
+        break;
+      }
+    }
+    if complete {
+      return true;
+    }
+  }
+
+  // check cols
+  for col in 0..5 {
+    let mut complete = true;
+    for row in 0..5 {
+      if !(numbers.contains(&board[row * 5 + col])) {
+        complete = false;
+        break;
+      }
+    }
+    if complete {
+      return true;
+    }
+  }
+  false
+}
+
+fn calculate_score(board: &Vec<u32>, played_numbers: &HashSet<u32>, last_number: u32) -> u32 {
+  board.into_iter().filter(|x| !played_numbers.contains(x)).sum::<u32>() * last_number
+}
