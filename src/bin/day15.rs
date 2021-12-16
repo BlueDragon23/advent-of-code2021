f573841ac683d256de846ccb7cb5df03fb2e11de
Aidan Goldthorpe <agoldthorpe@atlassian.com>
2021-12-17 09:01:25 +1100

    Day 15 part 2 done _very_ slowly

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,13 +1,11 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::collections::VecDeque;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::thread::current;
 
-use advent_of_code2021::Coordinate;
 use advent_of_code2021::get_adjacent_points;
+use advent_of_code2021::Coordinate;
 use itertools::Itertools;
 
 const PART: u32 = 2;
@@ -27,8 +25,16 @@ fn main() {
 fn find_path(cave: &Vec<Vec<u32>>) -> u32 {
     let mut visited = HashSet::new();
     let mut distances = HashMap::new();
-    let row_count = if PART == 1 { cave.len() } else { cave.len() * 5 };
-    let col_count = if PART == 1 { cave[0].len() } else { cave[0].len() * 5 };
+    let row_count = if PART == 1 {
+        cave.len()
+    } else {
+        cave.len() * 5
+    };
+    let col_count = if PART == 1 {
+        cave[0].len()
+    } else {
+        cave[0].len() * 5
+    };
     let mut current = Coordinate { row: 0, col: 0 };
     let mut current_distance = 0;
     distances.insert(current, 0);
@@ -46,17 +52,20 @@ fn find_path(cave: &Vec<Vec<u32>>) -> u32 {
         current = find_next_node(&distances, &visited);
         current_distance = *distances.get(&current).unwrap();
         visited.insert(current);
-        if visited.len() % 1000 == 0 {
+        if visited.len() % 10000 == 0 {
             println!("Visited {}/{} nodes", visited.len(), row_count * col_count);
         }
-        if current.row == row_count - 1  && current.col == col_count - 1 {
+        if current.row == row_count - 1 && current.col == col_count - 1 {
             // found the target
             break current_distance;
         }
     }
 }
 
-fn find_next_node(distances: &HashMap<Coordinate, u32>, visited: &HashSet<Coordinate>) -> Coordinate {
+fn find_next_node(
+    distances: &HashMap<Coordinate, u32>,
+    visited: &HashSet<Coordinate>,
+) -> Coordinate {
     distances
         .into_iter()
         .filter(|(c, _)| !visited.contains(c))
@@ -72,5 +81,11 @@ fn get_risk(cave: &Vec<Vec<u32>>, coordinate: Coordinate) -> u32 {
     let base_coordinate_col = coordinate.col % col_count;
     let initial = cave[base_coordinate_row][base_coordinate_col];
     let diff = (coordinate.row / row_count) + (coordinate.col / col_count);
-    (initial + (diff as u32)) % 10
+    let mut result = initial + (diff as u32);
+    if result >= 10 {
+        result -= 9;
+    }
+    // println!("row:{},col:{},base_row:{},base_col:{},base_val:{},diff:{},final:{}",
+    // coordinate.row, coordinate.col, base_coordinate_row, base_coordinate_col, initial, diff, result);
+    result
 }
