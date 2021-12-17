89f58198e4a25857caf29366ef5942f114cc2bf8
Aidan Goldthorpe <agoldthorpe@atlassian.com>
2021-12-17 15:25:01 +1100

    Day 16 part 2

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -26,57 +26,76 @@ fn main() {
         })
         .flatten()
         .collect_vec();
-    let final_state = parse_packet(&input, State { index: 0, version_sum: 0 });
+    let (final_state, result) = parse_packet(&input, State { index: 0, version_sum: 0 });
     println!("Part 1: {}", final_state.version_sum);
+    println!("Part 2: {}", result);
 }
 
-fn parse_packet(line: &Vec<char>, mut state: State) -> State {
+fn parse_packet(line: &Vec<char>, mut state: State) -> (State, usize) {
     let version = convert_from_binary(read_n_bits(line, state.index, 3));
     state.version_sum += version;
     state.index += 3;
     let type_id = convert_from_binary(read_n_bits(line, state.index, 3));
     state.index += 3;
-    if type_id == 4 {
-        state = parse_literal(line, state);
-    } else {
-        state = parse_operator(line, state);
+    match type_id {
+      4 => parse_literal(line, state),
+      op => parse_operator(line, state, op)
     }
-    state
 }
 
-fn parse_literal(line: &Vec<char>, mut state: State) -> State {
+fn parse_literal(line: &Vec<char>, mut state: State) -> (State, usize) {
   let mut chunk = read_n_bits(line, state.index, 5);
+  let mut chunks = vec![chunk[1..5].to_string()];
   state.index += 5;
   while chunk.chars().next().unwrap() == '1' {
     chunk = read_n_bits(line, state.index, 5);
+    chunks.push(chunk[1..5].to_string());
     state.index += 5;
   }
-  state
+  let literal = convert_from_binary(chunks.join(""));
+  (state, literal)
 }
 
-fn parse_operator(line: &Vec<char>, mut state: State) -> State {
+fn parse_operator(line: &Vec<char>, mut state: State, operator: usize) -> (State, usize) {
   let length_type_id = line[state.index];
   state.index += 1;
-  match length_type_id {
+  let (new_state, subpackets) = match length_type_id {
     '0' => {
       let bit_length = convert_from_binary(read_n_bits(line, state.index, 15));
       state.index += 15;
       let target_index = state.index + bit_length;
+      let mut results = vec![];
       while state.index != target_index {
-        state = parse_packet(line, state);
+        let output = parse_packet(line, state);
+        state = output.0;
+        results.push(output.1);
       }
-      state
+      (state, results)
     },
     '1' => {
       let packet_length = convert_from_binary(read_n_bits(line, state.index, 11));
       state.index += 11;
+      let mut results = vec![];
       for _ in 0..packet_length {
-        state = parse_packet(line, state);
+        let output = parse_packet(line, state);
+        state = output.0;
+        results.push(output.1);
       }
-      state
+      (state, results)
     },
     _ => panic!("Oopsy woopsy")
-  }
+  };
+  let result = match operator {
+    0 => subpackets.into_iter().sum(),
+    1 => subpackets.into_iter().product(),
+    2 => subpackets.into_iter().min().unwrap(),
+    3 => subpackets.into_iter().max().unwrap(),
+    5 => if subpackets[0] > subpackets[1] { 1 } else { 0 },
+    6 => if subpackets[0] < subpackets[1] { 1 } else { 0 },
+    7 => if subpackets[0] == subpackets[1] { 1 } else { 0 },
+    _ => panic!("Naughty operator")
+  };
+  (new_state, result)
 }
 
 fn convert_from_binary(text: String) -> usize {
