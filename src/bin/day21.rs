909c685f167788d80951e61e292922bbe6e49e52
Aidan Goldthorpe <aidan.goldthorpe@gmail.com>
2021-12-21 17:47:45 +1100

    Day 21 part 2 done

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,137 +1,129 @@
 use std::cmp::max;
-use std::cmp::min;
-use std::collections::HashSet;
-use std::collections::VecDeque;
-use std::fs;
-use std::io::BufRead;
+use std::collections::HashMap;
+use std::hash::Hash;
 
 use itertools::Itertools;
-use reformation::Reformation;
 
-const PART: u32 = 2;
 const P1_START: u32 = 2;
 const P2_START: u32 = 1;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 struct State {
     p1_position: u32,
     p1_score: u32,
     p2_position: u32,
     p2_score: u32,
-    dice_number: u32,
-    dice_rolls: u32,
-    p1_turn: bool,
 }
 
 fn main() {
-    let mut state = State {
+    let state = State {
         p1_position: P1_START,
         p2_position: P2_START,
         p1_score: 0,
         p2_score: 0,
-        dice_number: 1,
-        dice_rolls: 0,
-        p1_turn: true,
     };
-    let mut states = vec![state];
-    let mut finished_states = Vec::new();
-    let end_score = if PART == 1 { 1000 } else { 21 };
+    let mut states = HashMap::new();
+    states.insert(state, 1);
+    let mut finished_states = HashMap::new();
     let rolls = generate_rolls();
-    println!("{:?}", rolls);
-    while state.p1_score < end_score && state.p2_score < end_score {
-        if PART == 1 {
-            state = step_1(state);
-        } else {
-            states = states
-                .into_iter()
-                .map(|s| step_2(s, &rolls))
-                .flatten()
-                .collect_vec();
-            finished_states.extend(states.clone().into_iter().filter(|s| s.p1_score >= 21 || s.p2_score >= 21));
-            states = states.into_iter().filter(|s| s.p1_score < 21 && s.p2_score < 21).collect_vec();
-            println!("Ongoing games {}, finished games {}", states.len(), finished_states.len());
-        }
+    // println!("{:?}", rolls);
+    let mut p1_turn = true;
+    while states.len() > 0 {
+        states = states
+            .into_iter()
+            .map(|(s, count)| {
+                let player_state = match p1_turn {
+                    true => (s.p1_position, s.p1_score),
+                    false => (s.p2_position, s.p2_score),
+                };
+                step_2(player_state, &rolls)
+                    .into_iter()
+                    .map(|((position, score), step_count)| {
+                        let new_state = match p1_turn {
+                            true => State {
+                                p1_position: position,
+                                p1_score: score,
+                                p2_position: s.p2_position,
+                                p2_score: s.p2_score,
+                            },
+                            false => State {
+                                p1_position: s.p1_position,
+                                p1_score: s.p1_score,
+                                p2_position: position,
+                                p2_score: score,
+                            },
+                        };
+                        (new_state, count * step_count)
+                    })
+                    .collect::<HashMap<_, _>>()
+            })
+            .fold(HashMap::new(), |mut m: HashMap<State, u128>, sub_state| {
+                sub_state.into_iter().for_each(|(s, c)| {
+                    *m.entry(s).or_insert(0) += c;
+                });
+                m
+            });
+        p1_turn = !p1_turn;
+        finished_states = finished_states
+            .into_iter()
+            .chain(states.clone().into_iter())
+            .filter(|(s, _)| s.p1_score >= 21 || s.p2_score >= 21)
+            .fold(HashMap::new(), |mut m, (s, c)| {
+                *m.entry(s).or_insert(0) += c;
+                m
+            });
+        states = states
+            .into_iter()
+            .filter(|(s, _)| s.p1_score < 21 && s.p2_score < 21)
+            .collect();
     }
-    println!("{}, {}, {}", state.p1_score, state.p2_score, state.dice_rolls);
-    if PART == 1 {
-        println!(
-            "Part 1: {}",
-            min(state.p1_score, state.p2_score) * state.dice_rolls
-        );
-    }
-    let (p1_wins, p2_wins): (Vec<_>, Vec<_>) =
-        finished_states.into_iter().partition(|s| s.p1_score > s.p2_score);
-    println!("Part 2: {}", max(p1_wins.len(), p2_wins.len()));
+    let (p1_wins, p2_wins): (Vec<_>, Vec<_>) = finished_states
+        .into_iter()
+        .partition(|(s, _)| s.p1_score > s.p2_score);
+    let p1_win_count: u128 = p1_wins.into_iter().map(|(_, count)| count).sum();
+    let p2_win_count: u128 = p2_wins.into_iter().map(|(_, count)| count).sum();
+    println!("Scores were {} and {}", p1_win_count, p2_win_count);
+    println!("Part 2: {}", max(p1_win_count, p2_win_count));
 }
 
-fn step_1(mut state: State) -> State {
-    let rolls = state.dice_number * 3 + 3;
-    if state.p1_turn {
-        state.p1_position = state.p1_position + rolls;
-        while state.p1_position > 10 {
-            state.p1_position -= 10;
-        }
-        state.p1_score += state.p1_position;
-    } else {
-        state.p2_position = state.p2_position + rolls;
-        while state.p2_position > 10 {
-            state.p2_position -= 10;
-        }
-        state.p2_score += state.p2_position;
-    }
-    state.dice_rolls += 3;
-    state.dice_number += 3;
-    if state.dice_number > 100 {
-        state.dice_number -= 100;
-    }
-    state.p1_turn = !state.p1_turn;
-    state
+fn get_game_count(states: &HashMap<State, u128>) -> u128 {
+    states.into_iter().map(|(_, c)| c).sum()
 }
 
-fn step_2(state: State, rolls: &Vec<u32>) -> Vec<State> {
-    match state.p1_turn {
-        true => {
-            rolls.into_iter().map(|r| {
-                let mut p1_position = state.p1_position + r;
-                while p1_position > 10 {
-                    p1_position -= 10;
-                }
-                State {
-                    p1_position: p1_position,
-                    p1_score: state.p1_score + p1_position,
-                    p2_position: state.p2_position,
-                    p2_score: state.p2_score,
-                    dice_number: 0,
-                    dice_rolls: state.dice_rolls + 3,
-                    p1_turn: false
-                }
-            }).collect_vec()
-        },
-        false => {
-            rolls.into_iter().map(|r| {
-                let mut p2_position = state.p2_position + r;
-                while p2_position > 10 {
-                    p2_position -= 10;
-                }
-                State {
-                    p1_position: state.p1_position,
-                    p1_score: state.p1_score,
-                    p2_position: p2_position,
-                    p2_score: state.p2_score + p2_position,
-                    dice_number: 0,
-                    dice_rolls: state.dice_rolls + 3,
-                    p1_turn: true
-                }
-            }).collect_vec()
-        }
-    }
+#[allow(dead_code)]
+fn print_game_count(states: &HashMap<State, u128>, finished_states: &HashMap<State, u128>) {
+    let ongoing_count = get_game_count(states);
+    let finished_count = get_game_count(finished_states);
+    let total_count = ongoing_count + finished_count;
+    println!(
+        "Ongoing games {}, finished games {}, total {}",
+        ongoing_count, finished_count, total_count
+    );
 }
 
-fn generate_rolls() -> Vec<u32> {
+fn step_2(player: (u32, u32), rolls: &HashMap<u32, u128>) -> HashMap<(u32, u32), u128> {
+    rolls
+        .into_iter()
+        .map(|(increment, &count)| {
+            let mut position = player.0 + increment;
+            while position > 10 {
+                position -= 10;
+            }
+            let score = player.1 + position;
+            ((position, score), count)
+        })
+        .collect::<HashMap<_, _>>()
+}
+
+// Find out how many universes there are for each value
+fn generate_rolls() -> HashMap<u32, u128> {
     vec![1, 2, 3]
         .into_iter()
         .cartesian_product(vec![1, 2, 3])
         .cartesian_product(vec![1, 2, 3])
         .map(|((a, b), c)| a + b + c)
-        .collect_vec()
+        .fold(HashMap::new(), |mut m, sum| {
+            *m.entry(sum).or_insert(0) += 1;
+            m
+        })
 }
